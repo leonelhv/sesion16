bb26b62cb5c11965d9aba2ed60716357b787ac47
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-11 10:20:03 -0500

    add text for country

--- src/app/components/pais/pais.component.ts
+++ src/app/components/pais/pais.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class PaisComponent {
   rutaActual!: string;
 
-  paises = {
+  paises: any = {
     peru: {
       texto:
         '-	Perú es un país de Sudamérica que abarca una sección del bosque del Amazonas y Machu Picchu, una antigua ciudad inca en las alturas de los Andes. La región en torno a Machu Picchu, incluido el Valle Sagrado, el Camino del Inca y la ciudad colonial de Cuzco, es rica en sitios arqueológicos. El Perú es uno de los países más variados del mundo. Un país multicultural, lleno de tradiciones, una laureada gastronomía y vastas reservas naturales.',
@@ -20,7 +20,7 @@ export class PaisComponent {
     },
     uruguay: {
       texto:
-        '- Chile es un país largo y angosto que se extiende por el borde occidental de Sudamérica, con más de 6,000 km de costa en el océano Pacífico. Santiago, su capital, se ubica en un valle rodeado de los Andes y la cordillera de la Costa. En la Plaza de Armas de la ciudad, bordeada de palmeras, se encuentra la catedral neoclásica y el Museo de Historia Nacional. El enorme Parque Metropolitano cuenta con piscinas, un jardín botánico y un zoológico',
+        '-	Uruguay es un país de Sudamérica conocido por su interior verde y costa bordeada de playas. La capital, Montevideo, se ubica en torno a la plaza Independencia, donde alguna vez hubo una ciudadela española. Esta lleva a la Ciudad Vieja, casas coloniales y el Mercado del Puerto con muchos restaurantes de carnes. La Rambla es un paseo costero con puestos de pescados, muelles y parques.',
     },
   };
 
